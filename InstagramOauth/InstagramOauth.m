f3c62d3dfd71a21c3fd58e297ad22e0e79867420
hupeng <phu@ilegendsoft.com>
2014-04-29 15:26:57 +0800

    Initial Commit

---
+++ InstagramOauth/InstagramOauth.m
@@ -0,0 +1,13 @@
+//
+//  InstagramOauth.m
+//  InstagramOauth
+//
+//  Created by hupeng on 14-4-29.
+//  Copyright (c) 2014年 hupeng. All rights reserved.
+//
+
+#import "InstagramOauth.h"
+
+@implementation InstagramOauth
+
+@end
---
+++ InstagramOauthTests/InstagramOauthTests.m
@@ -0,0 +1,34 @@
+//
+//  InstagramOauthTests.m
+//  InstagramOauthTests
+//
+//  Created by hupeng on 14-4-29.
+//  Copyright (c) 2014年 hupeng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface InstagramOauthTests : XCTestCase
+
+@end
+
+@implementation InstagramOauthTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
